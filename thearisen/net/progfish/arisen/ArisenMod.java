af059e731f6479a70643df57c0bff889db295b1d
TJ Jaksa <tgamer27@gmail.com>
2013-08-02 00:46:15 -0400

    All events in mod class are added.

--- thearisen/net/progfish/arisen/ArisenMod.java
+++ thearisen/net/progfish/arisen/ArisenMod.java
@@ -1,5 +1,46 @@
 package net.progfish.arisen;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.ServerStarting;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.FMLServerStoppedEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.MOD_VERSION)
 public class ArisenMod {
 
+	@Instance(ModInfo.MOD_ID)
+	public static ArisenMod instance;
+	
+	@EventHandler
+	public void serverStarting(FMLServerStartingEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void serverStop(FMLServerStoppedEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void load(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void modsLoaded(FMLPostInitializationEvent event) {
+		
+	}
+	
 }
--- thearisen/net/progfish/arisen/ModInfo.java
+++ thearisen/net/progfish/arisen/ModInfo.java
@@ -2,4 +2,8 @@ package net.progfish.arisen;
 
 public class ModInfo {
 
+	public static final String MOD_ID = "arisen";
+	public static final String MOD_NAME = "The Arisen";
+	public static final String MOD_VERSION = "v0.0.1";
+	
 }
